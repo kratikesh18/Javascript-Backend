8b28ed33b1d65151029804edfde85ffe4487c9c0
kratikesh18 <im.kartikesh18@gmail.com>
2024-01-12 17:53:05 +0530

    Finished Backend Setup

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'
+
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME , 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+
+// updloading the file to the cloudinary 
+const updloadFileToCloud = async(localFilePath) => {
+    try {
+      if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath,{
+        resource_type:"auto"
+      })
+      // file is uploaded to the cloudinary 
+      console.log( "file is uploaded " , response.url)
+      return response 
+      
+    } catch (error) {
+      fs.unlinkSync(localFilePath)    //removes the locally saved temp file 
+      return null;
+    }
+}
+
+// exporting the uploadfile function 
+export {updloadFileToCloud}
