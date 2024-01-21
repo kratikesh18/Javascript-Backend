5c8f6caade264871610c10bb55bdee379310870a
kratikesh18 <im.kartikesh18@gmail.com>
2024-01-21 19:51:02 +0530

    added and fiexed logout route

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -25,6 +25,6 @@ router.route("/login").post(loginUser)
 
 // secured paths (routes)
 
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT , logoutUser)
 
 export default router
