3df53f44abdcaa089bad6ee159f4e51fdeb337c2
kratikesh18 <im.kartikesh18@gmail.com>
2024-01-22 23:49:35 +0530

    added the route for refreshToken endpoint

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
+import { loginUser, logoutUser, regenrateToken, registerUser } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -24,7 +24,7 @@ router.route("/register" ).post(
 router.route("/login").post(loginUser)
 
 // secured paths (routes)
-
 router.route("/logout").post(verifyJWT , logoutUser)
+router.route("/refresh-token").post(regenrateToken)
 
 export default router
